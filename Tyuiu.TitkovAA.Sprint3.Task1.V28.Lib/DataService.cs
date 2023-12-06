a7b827df2a24a60bca8f5485f684c2b16c2b85d6
Антон <vip.tank2005@outlook.com>
2023-12-06 11:51:47 +0500

    Task1

---
+++ Tyuiu.TitkovAA.Sprint3.Task1.V28.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+
+namespace Tyuiu.TitkovAA.Sprint3.Task1.V28.Lib
+{
+    public class DataService : ISprint3Task1V28
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double a = 0;
+            while (startValue<= stopValue)
+            {
+                a += (Math.Pow(value, startValue) + 0.25) * Math.Cos(startValue);
+                startValue++;
+            }
+
+
+
+
+            return a;
+        }
+    }
+}
---
+++ Tyuiu.TitkovAA.Sprint3.Task1.V28.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TitkovAA.Sprint3.Task1.V28.Lib;
+
+namespace Tyuiu.TitkovAA.Sprint3.Task1.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dt = new DataService();
+            Assert.AreEqual(-0.186,Math.Round(dt.GetSumSeries(0.5,1,18),3));
+        }
+    }
+}
